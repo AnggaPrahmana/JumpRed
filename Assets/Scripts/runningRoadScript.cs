e2b2bb2aeb40d8dfc1d0d4821fd0c9ab977d3edb
ACER\Angga <anggap820@gmail.com>
2017-10-08 17:15:42 +0700

    Initial Commit

---
+++ Assets/Scripts/runningRoadScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class runningRoadScript : MonoBehaviour {
+
+	public Transform targetSpawner;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y <= -100){
+			transform.position = new Vector3(targetSpawner.position.x,targetSpawner.position.y+145,targetSpawner.position.z);
+		}	
+		else {
+			float moveY = transform.position.y;
+			moveY -= 0.5f;
+			transform.position = new Vector3(transform.position.x,moveY,transform.position.z);
+		}	
+	}
+}
