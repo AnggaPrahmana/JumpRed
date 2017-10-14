cba2a3409dbdfdfb342a2ed6834e3bcfa01b2f57
DESKTOP\GAMERS <syahbri.gazzwhite@gmail.com>
2017-10-14 09:35:32 +0700

    no message

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameManager : MonoBehaviour {
 
 	// Use this for initialization
 	public static bool gamePause;
+	public Text score;
+	public Text Highscore;
+	public float scoreCount;
+	public float HighscoreCount;
+	public float scoreperSecond;
+	public bool scoreIncreasing;
 	void Start () {
-		
+		if(PlayerPrefs.HasKey("Highscore")){
+			HighscoreCount = PlayerPrefs.GetFloat("Highscore");
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(!gameManager.gamePause){
 		
+		if(scoreIncreasing){
+			scoreCount += scoreperSecond + 2;
+		}
+		
+		if(scoreCount > HighscoreCount){
+			HighscoreCount = scoreCount;
+			PlayerPrefs.SetFloat("Highscore", HighscoreCount);
+		}
+		score.text = "" + Mathf.Round (scoreCount);
+		Highscore.text = "" + Mathf.Round (HighscoreCount);
+
+	}
 	}
 
 	public void test(){
---
+++ Assets/Scripts/obScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obScript : MonoBehaviour {
+	public Sprite[] arrayObstacle;
+
+	public float speed = 20f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!gameManager.gamePause){
+		transform.position = new Vector3 (transform.position.x, transform.position.y - speed, transform.position.z);
+			if(transform.position.y <= -100){
+				transform.position = new Vector3(transform.position.x,transform.position.y+145,transform.position.z);
+			}
+
+}
+	}
+}
