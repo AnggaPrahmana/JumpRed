ecf1a29baa198d7f3d8f59794ef1cd1badf7ac88
DESKTOP\GAMERS <syahbri.gazzwhite@gmail.com>
2017-10-17 15:54:01 +0700

    no message

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -6,19 +6,18 @@ using UnityEngine.UI;
 public class gameManager : MonoBehaviour {
 
 	// Use this for initialization
-<<<<<<< HEAD
+// HEAD
 	public static bool gamePause=false;
 	public static bool loseGame=false;
 	public static bool twistTime=false;
-=======
-	public static bool gamePause;
+
 	public Text score;
 	public Text Highscore;
 	public float scoreCount;
 	public float HighscoreCount;
 	public float scoreperSecond;
 	public bool scoreIncreasing;
->>>>>>> cba2a3409dbdfdfb342a2ed6834e3bcfa01b2f57
+// cba2a3409dbdfdfb342a2ed6834e3bcfa01b2f57
 	void Start () {
 		if(PlayerPrefs.HasKey("Highscore")){
 			HighscoreCount = PlayerPrefs.GetFloat("Highscore");
@@ -27,12 +26,12 @@ public class gameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
+// HEAD
 		if(Input.GetKeyDown(KeyCode.Space)){
 			loseGame = !loseGame;
 			Debug.Log(loseGame);
 		}
-=======
+
 		if(!gameManager.gamePause){
 		
 		if(scoreIncreasing){
@@ -47,7 +46,7 @@ public class gameManager : MonoBehaviour {
 		Highscore.text = "" + Mathf.Round (HighscoreCount);
 
 	}
->>>>>>> cba2a3409dbdfdfb342a2ed6834e3bcfa01b2f57
+// cba2a3409dbdfdfb342a2ed6834e3bcfa01b2f57
 	}
 
 	public void test(){
